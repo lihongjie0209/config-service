4dfa7ce422124b3d99814645f938da98b70c5b3c
李宏杰 <lihongjie0209@gmail.com>
2026-09-01 07:21:02 +0800

    fix(authz): forward PSK caller credential

--- internal/transport/http/middleware.go
+++ internal/transport/http/middleware.go
@@ -270,7 +270,8 @@ func Authentication(service *auth.Service, logger *slog.Logger, cfg config.Auth)
 				return
 			}
 			c.Set("subject", "psk")
-			c.Request = c.Request.WithContext(platformprincipal.WithContext(c.Request.Context(), platformprincipal.Principal{ID: "config-service:psk", Type: platformprincipal.TypeServiceAccount}))
+			ctx := platformprincipal.WithContext(c.Request.Context(), platformprincipal.Principal{ID: "config-service:psk", Type: platformprincipal.TypeServiceAccount})
+			c.Request = c.Request.WithContext(platformauthz.WithCallerCredential(ctx, c.GetHeader("Authorization")))
 			c.Next()
 			return
 		}
